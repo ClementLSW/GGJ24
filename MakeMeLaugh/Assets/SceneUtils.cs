8685ba5582bedbc11db5deee13659713aea6f82b
IcedKlementi <117425643+IcedKlementi@users.noreply.github.com>
2024-01-28 12:48:57 +0800

    bounce logic and also some scene loading stuff

---
+++ MakeMeLaugh/Assets/SceneUtils.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneUtils : MonoBehaviour
+{
+    private static readonly Stack<Scene> LoadedScenes = new();
+    
+    public static void LoadSceneAdditive(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        LoadedScenes.Push(SceneManager.GetActiveScene());
+    }
+
+    public static void UnloadLastScene()
+    {
+        SceneManager.UnloadSceneAsync(LoadedScenes.Pop());
+    }
+    
+}
